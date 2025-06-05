b97b2b7ff969346d1a69bf0c32f5d3dfa2a67670
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 16:44:31 -0300

    feat: Adiciona novos elementos ao array e altera o arquivo README.md

---
+++ tarefas/exercicio01.js
@@ -0,0 +1,13 @@
+let promocoes = [
+    "teclado mecânico",
+    "mouse gamer",
+    "monitor ultrawide",
+    "cadeira ergonômica",
+    "webcam HD",
+    "fone bluetooth",
+    "notebook i5",
+    "hub USB"
+  ];
+  
+  promocoes.push (`SSD 1TB`, `mesa digitalizadora`);
+  console.log(`Os produtos atualizados são ${promocoes}`);
