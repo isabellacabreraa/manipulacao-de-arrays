6eb022ae7618689bf19fcbb277f4d4b5cf7fa91f
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 17:48:06 -0300

    feat: Cria e altera o arquivo exercicio12.js e edita o arquivo README.md

---
+++ exercicio12.js
@@ -0,0 +1,9 @@
+let palavrasChave = [
+    "desenvolvimento",
+    "frontend",
+    "javascript",
+    "css",
+    "react"
+  ];
+
+  console.log(`Palavras chave como string: ${palavrasChave.join(' - ')}`);
