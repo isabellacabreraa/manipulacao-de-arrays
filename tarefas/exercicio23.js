c0c715286cffbd4eed10c3bb7d16407f023155a3
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 19:17:51 -0300

    feat: Cria e altera o arquivo exercicio23.js e edita o arquivo README.md

---
+++ tarefas/exercicio23.js
@@ -0,0 +1,15 @@
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+
+//Colocando em ordem alfabética
+let candidatosOrdenados = candidatos.sort();
+console.log("Candidatos:");
+console.table(candidatosOrdenados);
