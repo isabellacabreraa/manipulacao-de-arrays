e0a121f0bd9bea259ebd304d4a0c7e3f110ba7f4
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 17:42:44 -0300

    feat: Cria e altera o arquivo exercicio11.js e edita o arquivo README.md

---
+++ exercicio11.js
@@ -0,0 +1,10 @@
+let ingredientes = [
+  "farinha de trigo",
+  "fermento",
+  "leite",
+  "ovos",
+  "açúcar",
+  "manteiga"
+];
+
+console.log(`Os ingredientes da receita são: ${ingredientes.join(", ")}`);
