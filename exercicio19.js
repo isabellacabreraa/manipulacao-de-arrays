9652dee9321b8f76ed141028911962e944a175ac
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 18:19:41 -0300

    feat: Cria e altera o arquivo exercicio19.js e edita o arquivo README.md

---
+++ exercicio19.js
@@ -0,0 +1,14 @@
+let codigos = [
+  "abc123",
+  "def456",
+  "ghi789",
+  "jkl012",
+  "mno345",
+  "pqr678",
+  "stu901",
+  "vwx234"
+];
+
+//Deixando tudo em maiúsculo
+let codigosMaiusculo = codigos.map(codigo => codigo.toUpperCase());
+console.log("Códigos em maiúsculo:", codigosMaiusculo)
