2b82b0ae138baada22016662b3cd0431cc4005fd
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 18:16:05 -0300

    feat: Cria e altera o arquivo exercicio18.js e edita o arquivo README.md

---
+++ exercicio18.js
@@ -0,0 +1,16 @@
+let hardware = [
+  "processador i7",
+  "placa-mãe",
+  "memória RAM 16GB",
+  "SSD 1TB"
+];
+
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone"
+];
+
+//Juntando as listas
+console.log("Lista de produtos atualizada:", hardware.concat(perifericos));
