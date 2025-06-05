09e9a6d7d73b2a1406dbc1ad6c5232f41ccb0d4d
Isabella <isabella.cabrera@aluno.senai.br>
2025-06-05 18:11:03 -0300

    feat: Cria e altera o arquivo exercicio17.js e edita o arquivo README.md

---
+++ exercicio17.js
@@ -0,0 +1,16 @@
+let usuariosSistemaA = [
+  "aline.souza",
+  "maria.ferreira",
+  "carlos.lima",
+  "ana.martins"
+];
+
+let usuariosSistemaB = [
+  "joao.silva",
+  "paulo.pereira",
+  "fernanda.alves",
+  "marcelo.cardoso"
+];
+
+//Juntando as listas
+console.log("Lista com usuários combinados:", usuariosSistemaA.concat(usuariosSistemaB));
